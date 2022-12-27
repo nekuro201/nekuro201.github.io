74df001f561d70d49563a958501d36f275293ddd
nekuro201 <guilherme.kodi.abe@gmail.com>
2022-12-28 03:47:06 +0900

    corrrent line possition

--- src/components/myfeatures.tsx
+++ src/components/myfeatures.tsx
@@ -10,65 +10,13 @@ import MysqlIcon from '../assets/mysql.svg';
 
 export function MyFeatures(){
 
-  const effectHoverSvg = 
-    `
-      w-5 
-      h-5
-      md:w-12
-      md:h-12
-    
-      fill-gray-100
-      group-hover:fill-black-700
-      group-hover:delay-75
-      delay-300   
-    `;
+  const effectHoverSvg = "w-5 h-5 md:w-12 md:h-12 fill-gray-100 group-hover:fill-black-700 group-hover:delay-75 delay-300";
 
-    const effectHoverSvg2 = 
-    `
-      w-5 
-      h-5
-      md:w-10
-      md:h-10
+  const effectHoverSvg2 = "w-5 h-5 md:w-10 md:h-10 fill-gray-100 group-hover:fill-black-700 group-hover:delay-75 delay-300 mt-1 md:mt-1.5";
 
-      fill-gray-100
-      group-hover:fill-black-700
-      group-hover:delay-75
-      delay-300 
-      mt-1
-      md:mt-1.5
-    `;
-
-    const effectHoverSvg3 = 
-    `
-      w-5 
-      h-5
-      md:w-12
-      md:h-12
-
-      fill-gray-100
-      group-hover:fill-black-700
-      group-hover:delay-75
-      delay-300 
-      mr-2
-    `;
+  const effectHoverSvg3 = "w-5 h-5 md:w-12 md:h-12 fill-gray-100 group-hover:fill-black-700 group-hover:delay-75 delay-300 mr-2";
   
-  const effectHoverCircle = 
-    `
-      flex
-      justify-center
-      items-center
-      bg-gray-700 
-      rounded-full 
-      w-8 
-      h-8
-      md:w-16
-      md:h-16 
-      group 
-      hover:bg-gray-100 
-      hover:-translate-y-2
-      transition 
-      duration-300
-    `;
+  const effectHoverCircle = "flex justify-center items-center bg-gray-700 rounded-full w-8 h-8 md:w-16 md:h-16 group hover:bg-gray-100 hover:-translate-y-2 transition duration-300";
 
   return(
 
