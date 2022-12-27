0a94ad588d3a6390f9cdbc78e895e8703a510fda
nekuro201 <guilherme.kodi.abe@gmail.com>
2022-12-28 04:31:38 +0900

    test 3

--- next.config.js
+++ next.config.js
@@ -4,11 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains: ['github.com'],
-    loader: "akamai",
-    path: "",
   },
-  basePath: "/nekuro201.github.io",
-  assetPrefix: "/nekuro201.github.io",
 }
 
 module.exports = nextConfig
