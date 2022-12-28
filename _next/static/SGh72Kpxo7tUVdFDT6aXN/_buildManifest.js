b7e046b5fce5ca626e01d11eca57e176e9d1e688
nekuro201 <guilherme.kodi.abe@gmail.com>
2022-12-28 03:45:26 +0000

    Deploying to gh-page from @ nekuro201/nekuro201.github.io@c17713117ec8eb2cc0aedf792cc74406bb15dc67 🚀

--- _next/static/SGh72Kpxo7tUVdFDT6aXN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/675-d114aeac394e2b22.js","static/chunks/pages/index-1de017ccb78f4ec7.js"],"/_error":["static/chunks/pages/_error-47e526b08b70a03d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
